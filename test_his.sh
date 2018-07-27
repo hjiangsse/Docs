3b931beba42e684d9ff6b42a6b34c3dce10b7d86
hjiangsse <hjiang@sse.com.cn>
2018-07-27 09:15:07 +0800

    test

--- test_his.sh
+++
@@ -1,9 +0,0 @@
-oldcmd=$(history | tail -n 1 | cut -d' '  -f5)
-oldpara=$(history | tail -n 1 | cut -d' '  -f6-)
-
-if echo $oldpara | grep -E "^'(.*)'$" ; then
-    echo "haha"
-else
-    echo "xixi"
-fi
-
